c08f1dd24cd25031fc8c9b68d58ed112c1854b96
王六锋 <2496436621@qq.com>
2019-04-09 10:16:53 +0800

    引入antd

--- src/container/home/index.jsx
+++ src/container/home/index.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './style.less';
-
-class Home extends Component{
-    render(){
-        return(
-            <div className="wlf-home">home</div>
+import { Button } from 'antd';
+class Home extends Component {
+    render() {
+        return (
+            <div className="wlf-home">
+                <Button type="primary">Primary</Button>
+            </div>
         )
     }
 }
