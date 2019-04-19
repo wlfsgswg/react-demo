ad55b0abb8147e5234e1e4c53f6547034d5628ca
王六锋 <2496436621@qq.com>
2019-04-19 15:11:19 +0800

    事件

--- src/container/login/index.jsx
+++ src/container/login/index.jsx
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import { Button } from 'antd';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+import LogoImage from "./../../image/logo.jpg";
 class Login extends Component {
-    render() {
-        return (
-            <div className="wlf-home">
-                <Link to='./silder'>
-                    <Button type="primary">登陆</Button>
-                </Link>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="wlf-home">
+        <Link to="./slider">
+          <Button type="primary">登陆</Button>
+        </Link>
+        <img src={LogoImage} alt="" />
+      </div>
+    );
+  }
 }
 
 export default Login;
