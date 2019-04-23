349b47a402f906802e517e5884041357ad072f92
王六锋 <2496436621@qq.com>
2019-04-23 13:50:44 +0800

    no message

--- src/container/login/index.jsx
+++ src/container/login/index.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import { Button } from "antd";
 import { Link } from "react-router-dom";
-import './style.less';
+import "./style.less";
+import ReactImage from '././../../image/react2.jpeg';
 class Login extends Component {
   render() {
     return (
       <div className="wlf-login">
-        <Link to="./slider">
-          <Button type="primary">登陆</Button>
-        </Link>
+        <div className="wlf-login-top" >
+          <div className="clearfix wlf-login-top-title">
+            <div className="l-left left p-r-15">
+              <img src={ReactImage} alt="" />
+            </div>
+            <div className="r-right right">
+              <div>React Demo</div>
+            </div>
+          </div>
+        </div>
+        <div className="wlf-login-content">
+        </div>
+        <div className="wlf-login-bottom" >
+        </div>
       </div>
     );
   }
