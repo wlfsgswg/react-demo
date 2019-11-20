1b162dc2c244f9432da8ba2ea838f49ee9a2ba6d
王六锋 <2496436621@qq.com>
2019-11-20 11:47:03 +0800

    no message

--- src/container/notFound/index.jsx
+++ src/container/notFound/index.jsx
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
-import './style.less';
-import { Button } from 'antd';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import "./style.less";
+import { Button } from "antd";
+import { withRouter } from "react-router-dom";
 
 class NotFound extends Component {
-    render() {
-        return (
-            <div className="wlf-notfound">
-                找不到你想要的页面
-            </div>
-        )
-    }
+  render() {
+    return <div className="wlf-notfound">找不到你想要的页面</div>;
+  }
 }
 
 export default withRouter(NotFound);
