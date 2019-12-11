6f56232d5f0fecf3bb981a7338daea9962353a4e
王六锋 <2496436621@qq.com>
2019-12-11 15:42:27 +0800

    no message

--- src/container/login/form/index.jsx
+++ src/container/login/form/index.jsx
@@ -1,65 +1,87 @@
-
-import React, { Component } from "./node_modules/react";
-import {
-    Form, Icon, Input, Button, Checkbox, message
-} from './node_modules/antd';
-import { withRouter } from './node_modules/react-router-dom';
+import React, { Component } from "react";
+import { Form, Icon, Input, Button, Checkbox, message } from "antd";
+import { withRouter } from "react-router-dom";
 
 import "./style.less";
 
 class LoginForm extends Component {
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                const { userName, password, remember } = values;
-                if (userName == "adminWlf" && password == "1") {
-                    localStorage.setItem("userName", "adminWlf");
-                    if (!remember) setTimeout(() => localStorage.setItem("userName", ""), 3000)
-                    //跳转首页
-                    this.props.history.push('/');
-                } else {
-                    message.error('用户名或密码错误')
-                }
-            }
-        });
-    }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        const { userName, password, remember } = values;
+        if (userName == "adminWlf" && password == "1") {
+          localStorage.setItem("userName", "adminWlf");
+          if (!remember)
+            setTimeout(() => localStorage.setItem("userName", ""), 3000);
+          //跳转首页
+          this.props.history.push("/");
+        } else {
+          message.error("用户名或密码错误");
+        }
+      }
+    });
+  };
 
-    render() {
-        const { getFieldDecorator } = this.props.form;
-        return (
-            <div className="wlf-login-form">
-                <Form onSubmit={this.handleSubmit} className="login-form">
-                    <Form.Item>
-                        {getFieldDecorator('userName', {
-                            rules: [{ required: true, message: '请输入用户名!' }],
-                        })(
-                            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="用户名" size="large" />
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('password', {
-                            rules: [{ required: true, message: '请输入密码!' }],
-                        })(
-                            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="密码" size="large" />
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('remember', {
-                            valuePropName: 'checked',
-                            initialValue: true,
-                        })(
-                            <Checkbox>记住密码</Checkbox>
-                        )}
-                        <a className="login-form-forgot" target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=2496436621&site=qq&menu=yes">获取账号</a>
-                        <Button type="primary" htmlType="submit" className="login-form-button" size="large">登陆</Button>
-                    </Form.Item>
-                </Form>
-            </div>
-        );
-    }
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div className="wlf-login-form">
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item>
+            {getFieldDecorator("userName", {
+              rules: [{ required: true, message: "请输入用户名!" }]
+            })(
+              <Input
+                prefix={
+                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                placeholder="用户名"
+                size="large"
+              />
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator("password", {
+              rules: [{ required: true, message: "请输入密码!" }]
+            })(
+              <Input
+                prefix={
+                  <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                type="password"
+                placeholder="密码"
+                size="large"
+              />
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator("remember", {
+              valuePropName: "checked",
+              initialValue: true
+            })(<Checkbox>记住密码</Checkbox>)}
+            <a
+              className="login-form-forgot"
+              target="_blank"
+              href="http://wpa.qq.com/msgrd?v=3&uin=2496436621&site=qq&menu=yes"
+            >
+              获取账号
+            </a>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+              size="large"
+            >
+              登陆
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
+  }
 }
 
-const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(LoginForm);
+const WrappedNormalLoginForm = Form.create({ name: "normal_login" })(LoginForm);
 
 export default withRouter(WrappedNormalLoginForm);
--- src/container/login/index.jsx
+++ src/container/login/index.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from "./node_modules/react";
+import React, { Component } from "react";
 import "./style.less";
-import ReactImage from '././../../image/react2.jpeg';
-import LoginForm from './form/index.jsx';
+import ReactImage from "././../../image/react2.jpeg";
+import LoginForm from "./form/index.jsx";
 class Login extends Component {
   render() {
     return (
       <div className="wlf-login">
-        <div className="wlf-login-top" >
+        <div className="wlf-login-top">
           <div className="clearfix wlf-login-top-title">
             <div className="l-left left p-r-15">
               <img src={ReactImage} alt="" />
@@ -15,21 +15,27 @@ class Login extends Component {
               <div>React Demo</div>
             </div>
           </div>
-          <div className="c666">React Demo 是西湖区最具影响力的 Web 设计规范</div>
+          <div className="c666">
+            React Demo 是西湖区最具影响力的 Web 设计规范
+          </div>
         </div>
         <div className="wlf-login-content">
           <LoginForm />
         </div>
-        <div className="wlf-login-bottom c999" >
+        <div className="wlf-login-bottom c999">
           <div className="wlf-login-bottom-flex">
             <span>帮助</span>
             <span className="p-l-20 p-r-20">隐私</span>
             <span>条款</span>
-             </div>
+          </div>
           <div>
             Copyright&nbsp;
-            <i className="iconfont icon-Copyright" style={{fontSize:"14px"}}></i>
-            &nbsp;2019 wlf前端技术出品</div>
+            <i
+              className="iconfont icon-Copyright"
+              style={{ fontSize: "14px" }}
+            ></i>
+            &nbsp;2019 wlf前端技术出品
+          </div>
         </div>
       </div>
     );
