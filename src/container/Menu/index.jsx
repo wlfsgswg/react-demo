b562308794e0ccd20aece2becc735eed48c843b4
王六锋 <2496436621@qq.com>
2019-12-12 15:08:56 +0800

    no message

--- src/container/Menu/index.jsx
+++ src/container/Menu/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { renderRoutes } from "react-router-config";
 import { classPrefix } from "./../../const/index.js";
 import { MenuLeft } from "./../../components/index.js";
-import { Layout } from "antd";
+import { Layout, Row, Col } from "antd";
 import "./index.less";
 const { Sider, Content, Header, Footer } = Layout;
 
@@ -13,18 +13,16 @@ export default class Menu extends Component {
   render() {
     return (
       <div className={`${classPrefix}-menu`}>
-        {/* <div className="menu-left">
-          <MenuLeft />
-        </div>
-        <div className="menu-right">
-          {renderRoutes(this.props.route.routes)}
-        </div> */}
         <Layout>
-          <Sider>Sider</Sider>
+          <Sider className="menu-sider">
+            <MenuLeft />
+          </Sider>
           <Layout>
-            <Header>Header</Header>
-            <Content>Content</Content>
-            <Footer>Footer</Footer>
+            <Header className="menu-header">Header</Header>
+            <Content className="menu-content">
+              {renderRoutes(this.props.route.routes)}
+            </Content>
+            {/* <Footer className="menu-footer">Footer</Footer> */}
           </Layout>
         </Layout>
       </div>
