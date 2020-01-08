c0662053cc79e85fd63dd938ddad178a313e355b
王六锋 <2496436621@qq.com>
2020-01-08 21:57:36 +0800

    随便玩玩

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./theme/common.less";
 import Root from "./container/Root.jsx";
 import { ConfigProvider } from "antd";
-
+console.log(1);
 export default class App extends Component {
   render() {
     return (
