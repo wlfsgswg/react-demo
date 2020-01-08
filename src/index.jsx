7396311944feee87eb541dcf2ab6d6d455849192
王六锋 <2496436621@qq.com>
2020-01-08 22:28:41 +0800

    heihei

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./theme/common.less";
 import Root from "./container/Root.jsx";
 import { ConfigProvider } from "antd";
+console.log(9000);
 export default class App extends Component {
   render() {
     return (
